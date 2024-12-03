64d9a77997f203a09f50fc3cbd183a1f448e362b
jacobcoro <xiaokourou@gmail.com>
2024-12-03 19:20:11 +0800

    shadcn setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
@@ -465,4 +467,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
