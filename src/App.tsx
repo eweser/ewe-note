506cbfe5ffe00844959f8ad4763cf8171a7eec7b
jacobcoro <xiaokourou@gmail.com>
2024-12-03 20:40:16 +0800

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import '@blocknote/core/fonts/inter.css';
 import { useCreateBlockNote } from '@blocknote/react';
 import { BlockNoteView } from '@blocknote/shadcn';
 import '@blocknote/shadcn/style.css';
+import { Layout } from './components/layout';
 
 function App() {
   const editor = useCreateBlockNote();
 
-  return <BlockNoteView editor={editor} />;
+  return (
+    <Layout>
+      <BlockNoteView editor={editor} />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,44 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import type { PropsWithChildren } from 'react';
+
+export function Layout({ children }: Readonly<PropsWithChildren>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <main className="flex flex-col p-4">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
