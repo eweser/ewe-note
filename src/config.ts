0667e718e0e26f920fd57c42007d3e6033fde48d
jacobcoro <xiaokourou@gmail.com>
2024-12-21 16:20:52 +0800

    remove unused config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
-const dummyUserName = 'dummy-user123';
-const dummyUserPass = 'dumdum';
-
 /**
  * Run the auth server from eweser-db project, cd ./packages/auth-server; npm run dev
  * If you are running on WSl, localhost:4444 might not work. Use ip addr show eth0 to find the local ip
@@ -19,17 +16,3 @@ export const dev = env === 'dev';
 export const ci = env === 'ci';
 
 export const showSignup = env !== 'prod';
-
-export const DEV_USERNAME =
-  import.meta.env.VITE_DEV_USERNAME ?? dev ? dummyUserName : '';
-export const DEV_PASSWORD =
-  import.meta.env.VITE_DEV_PASSWORD ?? dev ? dummyUserPass : '';
-
-/**
- * Run the rtc server from eweser-db project, cd ./test-rpc-server; npm run serve
- * If you are running on WSl, localhost:4444 might not work. Use ip addr show eth0 to find the local ip
- *
- * Note: Could not get webrtc working across browsers so instead we are going to use the ySweet websockets to sync
- */
-const localWebRtcServer = 'ws://172.31.42.92:4444/';
-export const WEB_RTC_PEERS = dev || ci ? [localWebRtcServer] : undefined;
