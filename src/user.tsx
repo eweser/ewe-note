9e601f1a2787cc84495c03cfe49434fca13be8ed
jacobcoro <xiaokourou@gmail.com>
2025-02-17 10:25:41 +0800

    trying to get profile working

--- src/user.tsx
+++ src/user.tsx
@@ -1,5 +1,5 @@
-import { Database } from '@eweser/db';
-import { useEffect, useState } from 'react';
+import type { Database } from '@eweser/db';
+import { useEffect, useMemo, useState } from 'react';
 
 export const useGetUserFromDb = (db: Database) => {
   const [user, setUser] = useState({
@@ -7,35 +7,85 @@ export const useGetUserFromDb = (db: Database) => {
     lastName: '',
     avatar: '',
   });
-  const profiles = db.getRooms('profiles');
-  const setUserProfile = async () => {
-    for (const room of profiles) {
-      console.log('profileRoom', room);
-      if (room.ydoc) {
-        await room.load();
-      }
-      const profile = room.getDocuments().getAllToArray()[0];
-      console.log({ profile });
-      let updatedUser = { ...user };
-      if (profile) {
-        if (profile.firstName) {
-          updatedUser.firstName = profile.firstName;
-        }
-        if (profile.lastName) {
-          updatedUser.lastName = profile.lastName;
-        }
-        if (profile.avatarUrl) {
-          updatedUser.avatar = profile.avatarUrl;
-        }
-      }
-      setUser(updatedUser);
-    }
-  };
-  for (const room of profiles) {
-    room.getDocuments().onChange(setUserProfile);
-  }
+  const [profilesLoaded, setProfilesLoaded] = useState(false);
+
   useEffect(() => {
-    setUserProfile();
-  }, []);
+    if (db) {
+      db.on('roomLoaded', () => {
+        if (db.getRooms('profiles').length > 1) {
+          const profileRoom = db
+            .getRooms('profiles')
+            .find((room) => room.publicAccess === 'read');
+          // console.log('profileRoom', profileRoom);
+          if (profileRoom?.ydoc) setProfilesLoaded(true);
+        }
+      });
+    }
+  }, [db]);
+
+  const PublicProfile = useMemo(
+    () =>
+      profilesLoaded
+        ? db
+            .getRooms('profiles')
+            .find((room) => room.publicAccess === 'read')
+            ?.getDocuments()
+        : null,
+    [db, profilesLoaded]
+  );
+  const [publicProfile, setPublicProfile] = useState(
+    PublicProfile?.get('default')
+  );
+  PublicProfile?.onChange(() =>
+    setPublicProfile(PublicProfile?.getAllToArray()[0])
+  );
+
+  const PrivateProfile = useMemo(
+    () =>
+      profilesLoaded
+        ? db
+            .getRooms('profiles')
+            .find((room) => room.publicAccess === 'private')
+            ?.getDocuments()
+        : null,
+    [db, profilesLoaded]
+  );
+
+  const [privateProfile, setPrivateProfile] = useState(
+    PrivateProfile?.get('default')
+  );
+  PrivateProfile?.onChange(() =>
+    setPrivateProfile(PrivateProfile?.getAllToArray()[0])
+  );
+
+  useEffect(() => {
+    const updatedUser = { ...user };
+    let updated = false;
+    for (const profile of [publicProfile, privateProfile]) {
+      if (profile) {
+        if (profile?.firstName) {
+          updatedUser.firstName = profile?.firstName;
+          updated = true;
+        }
+        if (profile?.lastName) {
+          updatedUser.lastName = profile?.lastName;
+          updated = true;
+        }
+        if (profile?.avatarUrl) {
+          updatedUser.avatar = profile?.avatarUrl;
+          updated = true;
+        }
+      }
+    }
+    if (updated) setUser(updatedUser);
+  }, [publicProfile, privateProfile, user]);
+  // console.log(PublicProfile?.getAll(), {
+  //   profilesLoaded,
+  //   publicProfile,
+  //   PublicProfile,
+  //   privateProfile,
+  //   PrivateProfile,
+  //   user,
+  // });
   return user;
 };
